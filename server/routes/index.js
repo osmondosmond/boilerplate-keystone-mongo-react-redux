40d7634a97584547bf4beb1275a290c685f14913
osmondosmond <osmond@tiket.com>
2018-04-04 13:30:07 +0700

    finishing tutorial

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
-// Then to get access to our API route we will use importer
 var keystone = require('keystone');
+// Then to get access to our API route we will use importer
 var importRoutes = keystone.importer(__dirname);
 // And finally set up the api on a route
 var routes = {
@@ -11,7 +11,7 @@ exports = module.exports = function (app) {
     // Get access to the API route in our app
     app.get('/api/recipe/', keystone.middleware.api, routes.api.recipe.list);
     // Set up the default app route to  http://localhost:3000/index.html
-    app.get('/index.html', function (req, res) {
+    app.get('/', function (req, res) {
     // Render some simple boilerplate html
     function renderFullPage() {
     // Note the div class name here, we will use that as a hook for our React code
